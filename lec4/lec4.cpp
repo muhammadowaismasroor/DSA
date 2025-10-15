b33a13095a80453be70cacdee02a45a8452cd811
Muhammad Owais <muhammadowais98746@gmail.com>
2025-10-15 00:29:36 -0700

    solved lec4 q2 and q3

--- lec4/lec4.cpp
+++ lec4/lec4.cpp
@@ -2,16 +2,44 @@
 using namespace std;
 int main()
 {
-    int n = 4;
+
+    // Lecture 04 Q# 01
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A';
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << ch;
+    //         ch += 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Lecture 04 Q# 02
+    // int n = 3;
+    // int val = 1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << val;
+    //         val++;
+    //     }
+    //     cout << endl;
+    // }
+    // return 0;
+
+    // Lecture 04 Q#03
+    int n = 3;
+    char ch = 'A';
     for (int i = 0; i < n; i++)
     {
-        char ch = 'A';
         for (int j = 0; j < n; j++)
         {
             cout << ch;
-            ch += 1;
+            ch++;
         }
         cout << endl;
     }
-    return 0;
 }
