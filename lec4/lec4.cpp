869b62ee91f3fb2a59a2f9fe5236790dc99bd105
Muhammad Owais <muhammadowais98746@gmail.com>
2025-10-16 03:56:43 -0700

    solved lec4 Q4,5and 6

--- lec4/lec4.cpp
+++ lec4/lec4.cpp
@@ -31,15 +31,50 @@ int main()
     // return 0;
 
     // Lecture 04 Q#03
-    int n = 3;
-    char ch = 'A';
+    // int n = 3;
+    // char ch = 'A';
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << ch;
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // // Lecture 04 Q#04
+    // int n = 3;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i+1; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Lecture 04 Q#05
+    // int n = 3;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << i + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Lecture 04 Q#06
+    int n = 4;
+    char ch = 65;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             cout << ch;
-            ch++;
         }
+        ch++;
         cout << endl;
     }
 }
