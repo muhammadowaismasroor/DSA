0248a068ad0fd115ea90e74cc7edfeb0f6b09776
Muhammad Owais <muhammadowais98746@gmail.com>
2025-11-12 03:04:19 -0800

    pracitve q 7

--- lec4/lec4.cpp
+++ lec4/lec4.cpp
@@ -80,13 +80,36 @@ int main()
     // }
 
     // Lecture 04 Q#07
-    int n = 5;
+    // int n = 5;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for(int j=1; j<=i+1; j++){
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
+    // Lecture 04 Q#08
+    //     int n = 4;
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         for (int j = i+1; j > 0; j--)
+    //         {
+    //             cout << j;
+    //         }
+    //         cout << endl;
+    // }
+
+    // Lecture 04 Q#08
+    int num = 1;
+    int n = 4;
     for (int i = 0; i < n; i++)
     {
-        for(int j=1; j<=i+1; j++){
-            cout << j;
+        for (int j = i + 1; j > 0; j--)
+        {
+            cout << num;
+            num++;
         }
         cout << endl;
     }
-
 }
